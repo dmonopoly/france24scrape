c1afb91b4231969baa3898e04541ba2c86f46a2d
David Zhang <dzhang21@gmail.com>
2012-12-03 18:54:35 -0800

    successfully grabs some headlines

--- main.py
+++ main.py
@@ -2,34 +2,22 @@ import webbrowser
 from bs4 import BeautifulSoup
 import urllib
 
-### Prep ###
 # Get a file-like object for the Python Web site's home page.
 site = "http://www.france24.com/fr/"
 f = urllib.urlopen(site)
+
+# webbrowser.open_new_tab(site) # open in browser tab
 
 # Read from the object, storing the page's contents in 'html_page'
 html_page = f.read()
 f.close()
 
-# Prep soup var
 soup = BeautifulSoup(html_page)
+those_items = soup.find_all("span", class_="title")
+for child in those_items:
+	print child.get_text().encode('utf-8')
+	print
 
-those_divs = soup.find("div", class_="title-provider")
-# res2 = ''.join(tmp.getText().strip() for tmp in those_divs.findAll('a'))
-those_divs.find_all("a")
-for child in those_divs:
-	grandchild = child.find_all("a")
-	print(grandchild)
-
-# print html_page
-
-#### Helper ####
-# def grab_data():
-	
-
-#### RUN ####
-# ids = grab_data()
-
-# for num in range(0,9): # assume >= 10
-# 	webbrowser.open_new_tab("http://www.facebook.com/"+ids[0]) # open in a tab
-	# webbrowser.open_new_tab("http://www.python.org") # open in a tab
+those_items = soup.find_all("h2", class_="title")
+for child in those_items:
+	print child.get_text().encode('utf-8')
